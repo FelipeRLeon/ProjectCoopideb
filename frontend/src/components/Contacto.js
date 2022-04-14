3ea50e3f8c151becbc22013e368ff85433704e31
Sirjulaco <Julian_2623@hotmail.com>
2022-04-14 01:29:26 -0500

    vista contacto asosiarse pqrst

--- frontend/src/components/Contacto.js
+++ frontend/src/components/Contacto.js
@@ -1,5 +1,33 @@
+import './css/Contacto.css';
 const Contacto = () => {
-    return <h1>Contacto</h1>;
+    return(
+      <main class="contenedor contacto no-pading">
+        <h1 class="centrar-texto">Contacto</h1>
+
+        <div class="contacto1">
+          <div class="contacto-texto1">
+            <p>
+              Dirección:Trasnversal 29# 37-21
+            </p>
+            <p>
+              Correo: Coopideb@hotmail.com
+            </p>
+          </div>
+          <div class="contacto-texto">
+            <p>
+              Ciudad:Bogotá 
+            </p>
+            <p>
+              Telefono:(601) 3684391 
+            </p>
+          </div>
+        </div>
+        <h1 class="centrar-texto">Mapa</h1>
+        <hr linea>
+        
+        </hr>
+      </main>
+    ) 
   };
   
   export default Contacto;
