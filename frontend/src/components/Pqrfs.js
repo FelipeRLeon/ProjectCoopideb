8cae202ef4f39f6b5f81e69cc80a55c1550e83eb
FelipeRLeon <feliiperestrepoleon@gmail.com>
2022-04-15 11:00:34 -0500

    Solucion errores

--- frontend/src/components/Pqrfs.js
+++ frontend/src/components/Pqrfs.js
@@ -14,52 +14,52 @@ const Pqrs = () => {
                 <legend>Peticiones, Quejas y Reclamos</legend>
                 <div className="contenedor-campos">
                     <div className="campo">
-                        <label className="campo__label" for="nombres">Nombres:</label>
+                        <label className="campo__label" htmlFor="nombres">Nombres:</label>
                         <input className="campo__field" type="text" placeholder="Tus Nombres" id="nombre"/>
                     </div>
                     <div className="campo">
-                        <label className="campo__label" for="apellidos">Apellidos:</label>
+                        <label className="campo__label" htmlFor="apellidos">Apellidos:</label>
                         <input className="campo__field" type="text" placeholder="Tus Apellidos" id="apellidos"/>
                     </div>
                     <div className="campo">
-                        <label className="campo__label" for="Tipo de documento">Tipo de documento:</label>
+                        <label className="campo__label" htmlFor="Tipo de documento">Tipo de documento:</label>
                         <select name="tipo documento">
 
                             <option>Pasaporte</option>
 
                             <option>Cedula</option>
 
-                            <option selected>T.I</option>
+                            <option defaultValue>T.I</option>
 
                         </select>
                     </div>
                     <div className="campo">
-                        <label className="campo__label" for="numerodoc">Número documento:</label>
+                        <label className="campo__label" htmlFor="numerodoc">Número documento:</label>
                         <input className="campo__field" type="number" placeholder="Numero documento" id="numerodoc"/>
                     </div>
                     <div className="campo">
-                        <label className="campo__label" for="correo">Correo electronico:</label>
+                        <label className="campo__label" htmlFor="correo">Correo electronico:</label>
                         <input className="campo__field" type="email" placeholder="Tu correo" id="correo"/>
                     </div>
                     <div className="campo">
-                        <label className="campo__label" for="dirección">Dirección:</label>
+                        <label className="campo__label" htmlFor="dirección">Dirección:</label>
                         <input className="campo__field" type="text" placeholder="Dirección" id="dirección"/>
                     </div>
                     <div className="campo">
-                        <label className="campo__label" for="celular">Celular:</label>
+                        <label className="campo__label" htmlFor="celular">Celular:</label>
                         <input className="campo__field" type="number" placeholder="(xxx)xxx xx-xx" id="celular"/>
                     </div>
                     <div className="campo">
-                        <label className="campo__label" for="ciudad">Ciudad:</label>
+                        <label className="campo__label" htmlFor="ciudad">Ciudad:</label>
                         <input className="campo__field" type="text" placeholder="ciudad" id="ciudad"/>
                     </div>
                     <div className="campo">
-                        <label className="campo__label" for="nombre">Hechos:</label>
+                        <label className="campo__label" htmlFor="nombre">Hechos:</label>
                         <textarea className="campo__field campo__field--textarea" id="hechos">
                         </textarea>
                     </div>
                     <div className="campo">
-                        <label className="campo__label" for="nombre">Peticiones:</label>
+                        <label className="campo__label" htmlFor="nombre">Peticiones:</label>
                         <textarea className="campo__field" id="peticiones">
                         </textarea>
                     </div>    
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,12 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));*/
+
+
+import { createRoot } from 'react-dom/client';
+import App from './App.js';
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="inicio" />);
