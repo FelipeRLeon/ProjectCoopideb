f88b931da1035ebf18d8580474a965a191285562
FelipeRLeon <feliiperestrepoleon@gmail.com>
2022-04-22 01:58:11 -0500

    Home responsive

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,12 +1,12 @@
 import './css/Home.css';
 const Home = () => {
     return (
-        <main className='contenedorServicios1'>            
-            <div className='credito-ser1'>
-              <div className='crediprima__imagen'>
+        <main className=''>            
+            <div className='row'>
+              <div className='col-sm-6 col-lg-4 col-12'>
                 <img alt='imagen-prima' src={require('../img/inicio.png')}></img>
               </div>
-              <div className='crediprima1'>
+              <div className='col-sm-6 col-lg-4 col-12'>
                 <div  className="d-flex justify-content-center">
                    <h2 className="titulo-coop w-auto">COOPIDEB</h2>
                 </div>
@@ -14,12 +14,10 @@ const Home = () => {
                 Somos una Cooperativa líder en el desarrollo economico y en la aplicación de principios y valores de la sociedad, prestamos servicios financieros y de bienestar social para el mejoramiento de la vida de nuestros asociados y sus familiares. 
                 </p>
             </div>    
-            </div>
-            <div className='img'>
-                <div className='fachada__imagen'>
+            <div className='col-sm-12 col-lg-4 col-12 fachada__imagen'>
                     <img alt='imagen-fachada' src={require('../img/fachada.png')}></img>
                 </div>
-            </div>      
+            </div>     
       </main>
     );
 };
