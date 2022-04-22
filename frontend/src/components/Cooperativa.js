e8ce800835dc1d932bbd762acbc8c3866aded002
FelipeRLeon <feliiperestrepoleon@gmail.com>
2022-04-22 02:35:58 -0500

    Coop y servisios responsive

--- frontend/src/components/Cooperativa.js
+++ frontend/src/components/Cooperativa.js
@@ -5,15 +5,15 @@ import pdf from '../data/Estatuto.pdf';
 
 const Cooperativa = () => {
     return (
-      <div>
+      <main>
       <div  className="d-flex justify-content-center">
         <h1 className="w-auto">Historia</h1>
       </div>
-        <div className="credito-ser1">
-          <div className="imagen-prima">
+        <div className="row">
+          <div className="col-sm-6 col-lg-6 col-12 imagen_historia">
           <img alt='imagen-prima' src={require('../img/historia 1.png')}></img>
           </div>
-          <div className="coop-historia">
+          <div className="col-sm-6 col-lg-6 col-12">
             <p>Por iniciativa de 22 exfuncionarios indemnizados del Distrito Especial de Bogotá DC., mediante ACTA DE CONSTITUCION del dia 23 Noviembre de 2002 debidamente registrada ante la CAMARA DE COMERCIO DE BOGOTA y reunidos los requisitos de ley se declaró constituida la COOPERATIVA
               MULTIACTIVA DE SERVICIOS Y SUMINISTROS Y SE IDENTIFICARA con la sigla COOPIDEB, con domicilio principal en la ciudad de Bogotá en la TV. 29 No.37-21, con el objeto principal de promover el desarrollo integral del ser humano, económico y cultural de sus asociados y el desarrollo de la comunidad mediante la aplicación y la práctica de los principios y valores cooperativos sobre la base de una eficiente administración. 
               Se aprobaron los Estatutos por unanimidad de los Socios Fundadores
@@ -27,35 +27,31 @@ const Cooperativa = () => {
 
         <div>
         <div  className="d-flex e-flex justify-content-center">
-        <h1 className="titulo-coop w-auto">Direccionamiento estratégico</h1>
-      </div>
+          <h1>Direccionamiento estratégico</h1>
+        </div>
           <div className="row">
-            <div className="coop-mision col-6">
+            <div className="col-sm-6 col-lg-6 col-12 coop-mision ">
               <h2>Mision</h2>
               <p>
               Somos una cooperativa de servicios y suministros que busca mediante  el desarrollo económico, mejorar la vida de sus asociados, familias y colaboradores, fomentando los principios y valores cooperativos, generando confianza, seguridad y responsabilidad solidaria.
               </p>
             </div>
-            <div className="coop-vision col-6">
+            <div className="col-sm-6 col-lg-6 col-12 coop-vision ">
               <h2>Vision</h2>
               <p>COOPIDEB será reconocida como la mejor Cooperativa crédito y bienestar social del sector solidario, empleando prácticas de crecimiento sostenibles y rentables.</p>
             </div>
           </div>
 
           <div className="row row-estatuto">
-          <div className="coop-estatuto col-6">
-          <Link target="_blank" className="coop-enlace" to={pdf}><button type="button" className="btn-coop w-100 h-100 btn btn-primary-outline">Estatuto</button></Link>
-          
-          
+            <div className="coop-estatuto col-sm-6 col-lg-6 col-12">
+              <Link target="_blank" className="coop-enlace" to={pdf}><button type="button" className="btn-coop w-100 h-100 btn btn-primary-outline">Estatuto</button></Link>
             </div>
-            <div className="coop-estructura col-6">
-            <Link download className="coop-enlace" to="/estructura"><button type="button" className="btn-coop w-100 btn btn-outline-primary">Estructura Organizacional</button></Link>
-            
-            </div>
-            
+            <div className="coop-estructura col-sm-6 col-lg-6 col-12">
+              <Link download className="coop-enlace" to="/estructura"><button type="button" className="btn-coop w-100 h-100 btn btn-outline-primary">Estructura Organizacional</button></Link>  
+            </div>  
           </div>
         </div>
-      </div>
+      </main>
     )
   };
   
--- frontend/src/components/Servicios.js
+++ frontend/src/components/Servicios.js
@@ -2,57 +2,57 @@ import './css/Servicios.css';
 const Servicios = () => {
     return(
       <main className='contenedorServicios'>
-          <div  className="d-flex justify-content-center">
-            <h1 className="titulo-coop w-auto">Créditos</h1>
+        <div  className="d-flex justify-content-center">
+          <h1 className="titulo-coop w-auto">Créditos</h1>
+        </div>
+        <div className='row'>
+          <div className='col-sm-6 col-lg-6 col-12 crediprima__imagenx'>
+            <img alt='imagen-prima' src={require('../img/prima-01 1 (1).png')}></img>
           </div>
-            <div className='credito-ser'>
-              <div className='crediprima__imagen'>
-                <img alt='imagen-prima' src={require('../img/prima-01 1 (1).png')}></img>
-              </div>
-              <div className='crediprima'>
-                <div  className="d-flex justify-content-center">
-                   <h2 className="titulo-coop w-auto">Crediprima</h2>
-                </div>
-                <p>
-                Crédito destinado a cubrir necesidades del asociado facilitando el pago a través de una cuota única para junio y diciembre. Descuento por nómina con entidades con las cuales COOPIDEB tenga convenio de descuento con libranza o por caja de acuerdo con las condiciones y garantías establecidas por Coopideb. 
-                </p>
-              </div>
-              <div className='crediprima__imagen'>
-                <img alt='imagen-casa'src={require('../img/casa-01 1 (1).png')} ></img>
-              </div>
-              <div className='crediprima'>
-              <div  className="d-flex justify-content-center">
-                   <h2 className="titulo-coop w-auto">Vivienda</h2>
-                </div>
-                <p>
-                Destinado a financiar la compra de vivienda familiar nueva o usada (incluye leasing)
-                Adquisición de Lote, construcción de vivienda en lote de propiedad del asociado y/o compra de cartera con hipoteca de un bien propiedad del asociado. Igualmente estará destinada a realizar mejoras, reparaciones o modificaciones de la vivienda de propiedad del asociado. 
-                </p>
-              </div>
-              <div className='crediprima'>
-              <div  className="d-flex justify-content-center">
-                   <h2 className="titulo-coop w-auto">Ordinario</h2>
-                </div>
-                <p>
-                Será de libre inversión para cubrir cualquier tipo de necesidad. Podrá tener varios créditos simultáneamente por esta línea siempre y cuando la capacidad de pago del asociado le permita atender las obligaciones de los mismos.
-                No se cobra estudio de crédito. 
-                </p>
-              </div>
-              <div className='estudianteimg'>
-                <img alt='imagen-casa'src={require('../img/ordinario-01 1 (1).png')} ></img>
-              </div>
-              <div className='crediprima'>
-              <div  className="d-flex justify-content-center">
-                   <h2 className="titulo-coop w-auto">Educativo</h2>
-                </div>
-                <p>
-                Para promover el desarrollo personal y profesional de los asociados y su grupo familiar (cónyuge o compañero(a) permanente, hijos, nietos y sobrinos) siempre y cuando se demuestre el parentesco. Se financia educación formal, especializaciones, Maestrías y MBA educación para el trabajo y el desarrollo humano, aprobados por el Ministerio de Educación Nacional o Secretaria de Educación, estudios en el exterior y útiles escolares e implementos tecnológicos y computadores. 
-                </p>
-              </div>
-              <div className='estudianteimg'>
-                <img alt='imagen-casa'src={require('../img/estudiante-01 1 (1).png')} ></img>
-              </div>
-            </div>      
+          <div className='col-sm-6 col-lg-6 col-12 crediprimax'>
+            <div>
+                <h2 className="titulo-coop w-auto">Crediprima</h2>
+            </div>
+            <p>
+            Crédito destinado a cubrir necesidades del asociado facilitando el pago a través de una cuota única para junio y diciembre. Descuento por nómina con entidades con las cuales COOPIDEB tenga convenio de descuento con libranza o por caja de acuerdo con las condiciones y garantías establecidas por Coopideb. 
+            </p>
+          </div>
+          <div className='col-sm-6 col-lg-6 col-12'>
+            <img alt='imagen-casa'src={require('../img/casa-01 1 (1).png')} ></img>
+          </div>
+          <div className='col-sm-6 col-lg-6 col-12'>
+          <div>
+                <h2 className="titulo-coop w-auto">Vivienda</h2>
+            </div>
+            <p>
+            Destinado a financiar la compra de vivienda familiar nueva o usada (incluye leasing)
+            Adquisición de Lote, construcción de vivienda en lote de propiedad del asociado y/o compra de cartera con hipoteca de un bien propiedad del asociado. Igualmente estará destinada a realizar mejoras, reparaciones o modificaciones de la vivienda de propiedad del asociado. 
+            </p>
+          </div>
+          <div className='col-sm-6 col-lg-6 col-12'>
+          <div>
+                <h2 className="titulo-coop w-auto">Ordinario</h2>
+            </div>
+            <p>
+            Será de libre inversión para cubrir cualquier tipo de necesidad. Podrá tener varios créditos simultáneamente por esta línea siempre y cuando la capacidad de pago del asociado le permita atender las obligaciones de los mismos.
+            No se cobra estudio de crédito. 
+            </p>
+          </div>
+          <div className='col-sm-6 col-lg-6 col-12'>
+            <img alt='imagen-casa'src={require('../img/ordinario-01 1 (1).png')} ></img>
+          </div>
+          <div className='col-sm-6 col-lg-6 col-12'>
+          <div>
+                <h2 className="titulo-coop w-auto">Educativo</h2>
+            </div>
+            <p>
+            Para promover el desarrollo personal y profesional de los asociados y su grupo familiar (cónyuge o compañero(a) permanente, hijos, nietos y sobrinos) siempre y cuando se demuestre el parentesco. Se financia educación formal, especializaciones, Maestrías y MBA educación para el trabajo y el desarrollo humano, aprobados por el Ministerio de Educación Nacional o Secretaria de Educación, estudios en el exterior y útiles escolares e implementos tecnológicos y computadores. 
+            </p>
+          </div>
+          <div className='col-sm-6 col-lg-6 col-12'>
+            <img alt='imagen-casa'src={require('../img/estudiante-01 1 (1).png')} ></img>
+          </div>
+        </div>
       </main>
       
     ) 
