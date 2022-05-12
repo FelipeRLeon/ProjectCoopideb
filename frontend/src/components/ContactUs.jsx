d885012ff5a1889f3c1dfaf13c6f4d69ffa44de1
FelipeRLeon <feliiperestrepoleon@gmail.com>
2022-05-12 00:06:14 -0500

    TesteoOk formulario pqrst

--- frontend/src/components/ContactUs.jsx
+++ frontend/src/components/ContactUs.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import emailjs from 'emailjs-com';
+import './css/pqrst.css';
 
 export const Mailer = () => {
 
@@ -13,20 +14,65 @@ export const Mailer = () => {
 
   return (
     <div className='div-form'>
-      <h1 className='title-form'>Contact Us</h1>
-      <form className='form-mail' onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name='user_name' />
-        <hr />
+      <form className='formulario' onSubmit={sendEmail}>
+      <fieldset>
+                    <legend>Peticiones, Quejas y Reclamos</legend>
+                    <div className="contenedor-campos">
+                        <div className="campo">
+                            <label className="campo__label" htmlFor="nombres">Nombres:</label>
+                            <input className="campo__field" name='pqrst_name' type="text" placeholder="Tus Nombres" id="nombre"/>
+                        </div>
+                        <div className="campo">
+                            <label className="campo__label" htmlFor="apellidos">Apellidos:</label>
+                            <input className="campo__field" name='pqrst_lastname' type="text" placeholder="Tus Apellidos" id="apellidos"/>
+                        </div>
+                        <div className="campo">
+                            <label className="campo__label" htmlFor="Tipo de documento">Tipo de documento:</label>
+                            <select name='pqrst_typeid'>
 
-        <label>Email</label>
-        <input type="email" name='user_email' />
-        <hr />
+                                <option>Pasaporte</option>
 
-        <label>Message</label>
-        <textarea name="user_message" id="" cols="30" rows="10"></textarea>
-        <hr />
-        <button>Send</button>
+                                <option>Cedula</option>
+
+                                <option defaultValue>T.I</option>
+
+                            </select>
+                        </div>
+                        <div className="campo">
+                            <label className="campo__label" htmlFor="numerodoc">Número documento:</label>
+                            <input className="campo__field" name='pqrst_id' type="number" placeholder="Numero documento" id="numerodoc"/>
+                        </div>
+                        <div className="campo">
+                            <label className="campo__label" htmlFor="correo">Correo electronico:</label>
+                            <input className="campo__field" name='pqrst_email' type="email" placeholder="Tu correo" id="correo"/>
+                        </div>
+                        <div className="campo">
+                            <label className="campo__label" htmlFor="dirección">Dirección:</label>
+                            <input className="campo__field" name='pqrst_address' type="text" placeholder="Dirección" id="dirección"/>
+                        </div>
+                        <div className="campo">
+                            <label className="campo__label" htmlFor="celular">Celular:</label>
+                            <input className="campo__field" name='pqrst_phone' type="number" placeholder="(xxx)xxx xx-xx" id="celular"/>
+                        </div>
+                        <div className="campo">
+                            <label className="campo__label" htmlFor="ciudad">Ciudad:</label>
+                            <input className="campo__field" name='pqrst_city' type="text" placeholder="ciudad" id="ciudad"/>
+                        </div>
+                        <div className="campo">
+                            <label className="campo__label" htmlFor="nombre">Hechos:</label>
+                            <textarea name="pqrst_hechos" className="campo__field campo__field--textarea" id="hechos">
+                            </textarea>
+                        </div>
+                        <div className="campo">
+                            <label className="campo__label" htmlFor="nombre">Peticiones:</label>
+                            <textarea name="pqrst_message" className="campo__field" id="peticiones">
+                            </textarea>
+                        </div>    
+                    </div> 
+                    <div className="alinear-derecha flex">
+                        <input type="submit" value="Enviar" className="boton wsm-100"/>
+                    </div>
+                </fieldset>
       </form>
       
     </div>
